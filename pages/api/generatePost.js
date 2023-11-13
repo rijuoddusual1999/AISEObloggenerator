7bb6437f029f54512135f986d14274d754586e5d
Riju Mondal <pilotrijurocks@gmail.com>
2023-11-13 15:22:01 +0530

    unsafe head

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,18 +1,67 @@
 import { Configuration, OpenAIApi } from 'openai';
  
 export default async function handler(req, res) {
+
+
     const config = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
-      });
- 
+    });
+
+
+    const {topic,keywords} = req.body;
+
+
+    /*const topic = "Top 10 tips for dog owners";
+    const keywords = "first time dog owners,German Sephered"*/
+
+
+     
+  
       const openai = new OpenAIApi(config);
-      const response = await openai.createCompletion({
+      
+      const postContentResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 0,
+            messages: [{
+              role: "system",
+              content: "You are a blog post generator"
+            },{
+              role: "user",
+              content: `Write a long and detailed SEO friendly blog post about ${topic}, that targets following keywords: ${keywords},
+              The content should be formatted in SEO friendly HTML,
+              limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li,ol, ul`
+            },],
+
+      });
+      
+      
+      
+      
+      
+      
+      /*const response = await openai.createCompletion({
         model: 'text-davinci-003',
         temperature: 0,
         max_tokens: 3600,
-        prompt: 'Generate a blog post about owing dogs'
-      });
-     console.log("response: ",response);
-  res.status(200).json({ post: response.data.choices});
+        prompt:  `Write a long and detailed SEO friendly blog post about ${topic}, that targets following keywords: ${keywords}
+        The content should be formatted in SEO friendly HTML 
+        The title must also include appropriate HTML title and meta description content
+        The return format must be a stringified JSON in the following formats:
+        {
+          "postContent": post content here 
+          "title": title goes here
+          "metaDescription": meta description goes here 
+        }`,
+      });*/
+     
+
+
+
+
+
+ 
+     console.log('response: ',postContentResponse.data.choices[0]?.message?.content);
+     /*res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text.split('\n').join('')) });*/
+
 
  }
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,28 +1,60 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
 
 export default function NewPost(props) {
   console.log('NEW POST PROPS: ', props);
+  const [postContent,setPostContent] = useState("");
 
-  const handleClick = async () => {
+  const[keywords,setKeywords] = useState("");
+  const[topic,setTopic] = useState("");
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+  
     const response = await fetch(`/api/generatePost`, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({topic, keywords}),
     });
-    
+  
     const json = await response.json();
-    console.log('RESULT: ', json);
-    
+    console.log('RESULT: ', json.post.postContent);
+    setPostContent(json.post.postContent);
   };
+  
 
   return (
     <div>
-      <h1>this is a new page</h1>
-      <button className="btn" onClick={handleClick}>
+      <form onSubmit={handleClick}>
+      <button type = "submit" className="btn">
         Generate
       </button>
+
+       <div>
+        <label>
+          <strong>
+            Generate a blog post:
+          </strong>
+        </label>
+        <textarea className="resize-none border border-slate-700 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(e) => setTopic(e.target.value)}/>
+       </div>
+       
+
+       <div>
+       <label>
+          <strong>
+            Targeting the following keywords:
+          </strong>
+        </label>
+        <textarea className="resize-none border border-slate-700 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)}/>
+
+       </div>
+       </form>
+      <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html: postContent}}/>
+      
     </div>
   );
 };

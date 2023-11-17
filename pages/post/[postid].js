ea03040e8ffaf70bd32d99b50e2cf0a87edeb685
Riju Mondal <pilotrijurocks@gmail.com>
2023-11-17 23:52:25 +0530

    unsafe head postid

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,6 +1,11 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+import clientPromise from "../../lib/mongodb.js";
+import { ObjectId } from "mongodb";
 
-export default function Post() {
+export default function Post(props) {
+    console.log('PROPS: ',props)
+
     return <div>
       <h1>
        this is post  
@@ -9,10 +14,42 @@ export default function Post() {
   }
   
 
-  export const getServerSideProps = withPageAuthRequired (() =>{
-    return{
-        props:{
+  Post.getLayout = function getLayout(page, pageProps) {
+    return <AppLayout {...pageProps}>{page}</AppLayout>;
+  };
+  
 
-        }
-    }
+  export const getServerSideProps = withPageAuthRequired ({
+    async getServerSideProps(ctx){
+      const userSession = await getSession(ctx.req, ctx.res);
+      const client = await clientPromise;
+      const db = client.db("BlogBraniac");
+      const user = await db.collection("users").findOne({
+        auth0Id : userSession.user.sub,
+      });
+      const post = await db.collection("posts").findOne({
+        _id: new ObjectId(ctx.params.postId),
+        userId: user._id,
+      });
+
+
+      if (!post) {
+        return {
+          notFound: true,
+        };
+      }
+
+    
+
+      return{
+        props:{
+          postContent: post.postContent,
+          title: post.title,
+          metaDescription: post.metaDescription,
+          keywords: post.keywords
+        },
+      };
+
+    },
   });
+
